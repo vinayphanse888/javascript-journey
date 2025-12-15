69df73b4f2256eb467cdfad022db77c5ad1ceb03
Vinay Phanse <vinayphanse14@gmail.com>
2025-12-15 07:45:29 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -8,6 +8,46 @@
 
 // true => 1; false => 0
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let value = 3;
+// let negValue = -value
+// // console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = "Vinay"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);  // 122
+// console.log(1 + 2 + "2");   // 32
+
+// console.log(3 + 4 * 5 % 3);     // use parenthesis 
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+
+
+
+
+
