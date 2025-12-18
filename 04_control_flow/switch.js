49d15da123df7e15cfc4ecda651f90c17f62f9cc
Vinay Phanse <vinayphanse14@gmail.com>
2025-12-18 14:08:00 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,55 @@
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "feb"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("february");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    case "june":
+        console.log("june");
+        break;
+    case "july":
+        console.log("july");
+        break;
+    case "august":
+        console.log("august");
+        break;
+    case "september":
+        console.log("september");
+        break;
+    case "october":
+        console.log("october");
+        break;
+    case "november":
+        console.log("november");
+        break;
+    case "december":
+        console.log("december");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
