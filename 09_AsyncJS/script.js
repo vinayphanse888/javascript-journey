c508efe8c2f814d1f0d086082f926bde393016bf
Vinay Phanse <vinayphanse14@gmail.com>
2025-12-31 14:49:42 +0000

    async

---
+++ 09_AsyncJS/script.js
@@ -0,0 +1,26 @@
+//sync and async kya hota hai ? 
+// SYNC =====> sync matlab ek ke baad dusra hoga, jab tak ek command complete naa ho, dusra shuru nahi hoga 
+
+// ASYNC ======> async matlab saare kaam ek sath shuru kardo, jiska answer pahle aajaye uska jawaab dedena
+
+// kaise pata chalta hai ki hum sync code likh rahe ya async 
+
+/*
+setTimeout 
+setInterval
+Promise                 -------> If this are used in code It is asynchronous code
+fetch
+axios
+XMLHttpRequest
+*/
+
+
+// async js hai kya ? 
+// js is not asynchronous
+// async ki poori kahaani.
+// single threading and multi threading 
+// callbacks 
+// promises 
+// then and catch
+// async await
+// 5 use cases ekdum real world waale
